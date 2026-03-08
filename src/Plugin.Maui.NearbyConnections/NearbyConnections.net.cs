e4e5b0559b463483fa5efb195a6e5185866033c2
phunkeler <paulhunkeler@gmail.com>
2026-03-08 16:12:53 -0400

    [Sonar] More supressions

--- src/Plugin.Maui.NearbyConnections/NearbyConnections.net.cs
+++ src/Plugin.Maui.NearbyConnections/NearbyConnections.net.cs
@@ -2,21 +2,24 @@ namespace Plugin.Maui.NearbyConnections;
 
 internal sealed partial class NearbyConnectionsImplementation
 {
+#pragma warning disable CA1822, S2325, S1144, S1172
     Task PlatformRequestConnectionAsync(NearbyDevice device)
-        => throw new NotImplementedException();
+        => throw new PlatformNotSupportedException("This functionality is not supported in this platform implementation.");
 
     Task PlatformRespondToConnectionAsync(NearbyDevice device, bool accept)
-        => throw new NotImplementedException();
+        => throw new PlatformNotSupportedException("This functionality is not supported in this platform implementation.");
 
     Task PlatformSendAsync(
         NearbyDevice device,
         byte[] data,
         IProgress<NearbyTransferProgress>? progress,
-        CancellationToken cancellationToken) => throw new NotImplementedException();
+        CancellationToken cancellationToken) => throw new PlatformNotSupportedException("This functionality is not supported in this platform implementation.");
+
 
     Task PlatformSendAsync(
         NearbyDevice device,
         string fileUri,
         IProgress<NearbyTransferProgress>? progress,
-        CancellationToken cancellationToken) => throw new NotImplementedException();
+        CancellationToken cancellationToken) => throw new PlatformNotSupportedException("This functionality is not supported in this platform implementation.");
+#pragma warning restore CA1822, S2325, S1144, S1172
 }
