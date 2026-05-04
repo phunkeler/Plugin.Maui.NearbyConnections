ccf6a9670c66b472c177d8a001c39147932ca477
phunkeler <paulhunkeler@gmail.com>
2026-05-03 23:47:59 -0400

    sonarqube: security

--- src/Plugin.Maui.NearbyConnections/Public/NearbyConnectionsOptions.net.cs
+++ src/Plugin.Maui.NearbyConnections/Public/NearbyConnectionsOptions.net.cs
@@ -4,5 +4,5 @@ public sealed partial class NearbyConnectionsOptions
 {
     private static partial string GetDefaultDisplayName() => string.Empty;
     private static partial string GetDefaultServiceId() => string.Empty;
-    private static partial string GetDefaultReceivedFilesDirectory() => Path.GetTempPath();
+    private static partial string GetDefaultReceivedFilesDirectory() => string.Empty;
 }
